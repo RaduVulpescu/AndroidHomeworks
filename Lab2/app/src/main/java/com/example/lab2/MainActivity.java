eb7953dd8b6076cbaa9043f7f3a4a22375ec7302
Radu Vulpescu <radu.vulpescu16@gmail.com>
2020-03-11 21:17:42 +0200

    Add empty menu, activity lifecycle support and landscape layout

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.lab2;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -21,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.i("Lab3", "OnCreate has been called and content view has been set");
+
         ListView listView = findViewById(R.id.listView);
 
         components.add(new Component("RTX 2080", 3000, "Nvidia", "Great video card"));
@@ -31,13 +37,80 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getTitles(components));
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        final TextView textView = findViewById(R.id.textView3);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, formatDescription(components.get(position)), Toast.LENGTH_SHORT).show();
+                textView.setText(formatDescription(components.get(position)));
+                //Toast.makeText(MainActivity.this, formatDescription(components.get(position)), Toast.LENGTH_SHORT).show();
             }
         });
+
+        Log.i("Lab3", "OnCreate has been terminated");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Log.i("Lab3", "onRestoreInstanceState has been terminated");
+        Log.i("Lab3", "onRestoreInstanceState has been terminated");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i("Lab3", "onStart has been terminated");
+        Log.i("Lab3", "onStart has been terminated");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        Log.i("Lab3", "onResume has been terminated");
+        Log.i("Lab3", "onResume has been terminated");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        Log.i("Lab3", "onPause has been terminated");
+        Log.i("Lab3", "onPause has been terminated");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i("Lab3", "onStop has been terminated");
+        Log.i("Lab3", "onStop has been terminated");
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.i("Lab3", "onSaveInstanceState has been terminated");
+        Log.i("Lab3", "onSaveInstanceState has been terminated");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        Log.i("Lab3", "onDestroy has been terminated");
+        Log.i("Lab3", "onDestroy has been terminated");
     }
 
     private static ArrayList<String> getTitles(ArrayList<Component> components) {
@@ -59,4 +132,6 @@ public class MainActivity extends AppCompatActivity {
 
         return sb.toString();
     }
+
+
 }
